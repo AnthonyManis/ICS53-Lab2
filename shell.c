fca1f1cabebce844b3ee61feced27ffbb62ef35e
AnthonyManis <anthonygmanis@gmail.com>
2016-04-22 04:31:52 -0700

    parseCommand and promptUser finished
    There was a small bug in parseCommand that was ruining everything. I was
    index operatoring a pointer the wrong way whoopsie. I fixed this and in
    the meantime finished writing parseCommand and promptUser

--- shell.c
+++ shell.c
@@ -5,10 +5,9 @@
 
 
 void promptUser();
-int parseCommand(char *line, size_t *n, char *tokens[]);
-int get_token(char **token, size_t *n, char *delims, char* str);
+int parseCommand(char *line, size_t *n, char ***tokens);
 void quit_command();
-void general_command(char *argv[]);
+void general_command(char **argv);
 
 // shows a prompt to the user, gets an input line,
 // calls parseCommand, then determines and calls
@@ -20,100 +19,89 @@ void promptUser() {
         char **argv = NULL;
         size_t line_size = 0;
         size_t n = 0;
+
         printf("prompt>");
         
-        int read;
-        if ( (read = getline(&line, &line_size, stdin)) > 0 ) {
-            if (line[read-1] == '\n') {
-                line[read-1] = '\0';
-            }
-        }
-        int argc = parseCommand(line, &n, argv);
+        getline(&line, &line_size, stdin);
+        int argc = parseCommand(line, &n, &argv);
         free(line);
-
-        // as long as the line wasn't blank
+        printf("%d tokens parsed\n", argc);
         if (argc > 0) {
+            int i;
+            for (i = 0 ; i < argc ; i++) {
+                printf("argv %d: %s\n", i, argv[i]);
+            }
             if (!strcmp(argv[0], "quit")) {
                 quit_command();
-
+                break;
             }
             else {
                 general_command(argv);
             }
         }
-        if (argc != -1){
+        if (argc != -1) {
             int i;
-            for (i = 0 ; i < n ; i++) {
+            for (i = 0 ; i < argc ; i++) {
                 free(argv[i]);
             }
         }
+        free(argv);
     }
 }
 
 // parses a line into command & any arguments,
-// storing command in (*tokens)[0] and arguments
-// each in the following elements
-// n is the size of the final array (for caller to free())
-// returns the tokens by reference
-// returns the number of tokens or -1 if allocation error
-int parseCommand(char *line, size_t *n, char *tokens[]) {
-    // line pointer can not be null
+// storing command and arguments as tokens in the (*tokens) array
+// n is the size of the final array
+// returns the number of tokens allocated or -1 if allocation error
+int parseCommand(char *line, size_t *n, char ***tokens) {
     if (!line)
         return -1;
-
-    // allocate initial space to tokens array
-    if (!tokens) {
+    if (!*tokens) {
         *n = 2;
-        tokens = malloc(*n * sizeof(char *));
-        if (!tokens)
-            return -1; // failed to allocate
+        *tokens = malloc(*n * sizeof(char*));
+        if (!*tokens)
+            return -1;
     }
 
-    // tokens in line are separated by whitespace, unless
-    // enclosed in quotation marks
-    char *tp = NULL; // token pointer
+    char *delims = " \t\n";
+    char *tp = line;
+    char *next_token = line;
     size_t token_size = 0;
-    get_token(&tp, &token_size, " \t\n", line);
-    int counter = 0;
-    while (tp)
-    {
-        // reallocate tokens array if necessary
-        if (counter >= *n) {
-            *n *= 2;
-            tokens = realloc(tokens, *n * sizeof(char*));
-            if (!tokens)
-                return -1; // failed to reallocate
+    size_t count = 0;
+
+    while (*next_token) {
+        if (count >= *n) {
+            *n = *n * 2;
+            *tokens = realloc(*tokens, *n * sizeof(char*));
+            if (!*tokens)
+                return -1;
         }
-        // always allocate for each token
-        tokens[counter] = malloc(strlen(tp) + 1);
-        if (!tokens[counter])
-            return -1; // failed to allocate
-        strcpy(tokens[counter], tp);
-        ++counter;
-
-        free(tp);
-        token_size = 0;
-        get_token(&tp, &token_size, " \t\n", line);
+        if (!strchr(delims, *next_token)) {
+            token_size++;
+            next_token++;
+        }
+        else {
+            (*tokens)[count] = malloc(token_size+1);
+            if (!(*tokens)[count])
+                return -1;
+            strncpy((*tokens)[count], tp, token_size);
+            (*tokens)[count][token_size] = '\0';
+            token_size = 0;
+            count++;
+            while(*next_token && strchr(delims, *next_token)){
+                next_token++;
+            }
+            tp = next_token;
+        }
     }
-    if (tp)
-        free(tp);
-
-    return counter;
-}
-
-// gets a token, seperated by any number of delims
-// returns number of characters read not including '\0'
-// token is stored with terminating '\0' into token
-// dynamically allocates token as necessary, caller must free() token
-int get_token(char **token, size_t *n, char *delims, char* str) {
-
+    return count;
 }
 
 void quit_command() {
 
 }
 
-void general_command(char *argv[]) {
+void general_command(char **argv) {
 
 }
 
